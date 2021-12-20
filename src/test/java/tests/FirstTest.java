5b144423bcadeac2d6b2c849b1f07dce546e42ee
Vasyl Fomiuk <vasya23fomiuk@gmail.com>
2021-12-20 15:21:06 -0500

    Update FirstTest.java

--- src/test/java/tests/FirstTest.java
+++ src/test/java/tests/FirstTest.java
@@ -16,15 +16,20 @@ public class FirstTest {
     @Test
     public void test1() throws MalformedURLException, InterruptedException {
         DesiredCapabilities caps = new DesiredCapabilities();
+        //we can specify capability name with MobileCapabilityType class
         caps.setCapability("deviceName", "Pixel 3");
         caps.setCapability("platformName", "Android");
         caps.setCapability("platformVersion", "10");
         caps.setCapability("automationName", "UiAutomator2");
+        //app under test
         caps.setCapability("app", "https://cybertek-appium.s3.amazonaws.com/calculator.apk");
         URL url = new URL("http://localhost:4723/wd/hub");
         driver = new AndroidDriver<>(url, caps);
+
+        //find element by id and click it
         driver.findElement(By.xpath("//*[@text='1']")).click();
         Thread.sleep(3000);
+
         driver.closeApp();
     }
 }
