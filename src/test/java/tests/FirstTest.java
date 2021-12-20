8db53cf18e0fcf4e3c9d0cd4899f80b59253f0e3
Vasyl Fomiuk <vasya23fomiuk@gmail.com>
2021-12-20 15:28:43 -0500

    Update FirstTest.java

--- src/test/java/tests/FirstTest.java
+++ src/test/java/tests/FirstTest.java
@@ -1,6 +1,7 @@
 package tests;
 
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileBy;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.jupiter.api.Test;
@@ -24,10 +25,20 @@ public class FirstTest {
         //app under test
         caps.setCapability("app", "https://cybertek-appium.s3.amazonaws.com/calculator.apk");
         URL url = new URL("http://localhost:4723/wd/hub");
+        //Chrome = ChromeDriver
+        //Android = AndroidDriver
+        //IOS = IOSDriver'
+        // WebElement -> MobileElement -> AndroidElement/IOSElement
+        // WebDriver -> RemoteWebDriver -> ChromeDriver
+        //                                 AppiumDriver -> AndroidDriver/IOSDriver
         driver = new AndroidDriver<>(url, caps);
 
+        // MobileBy is a child of By class
+        //AccessibilityId new locator strategy. Available only in mobile apps
         //find element by id and click it
         driver.findElement(By.xpath("//*[@text='1']")).click();
+        driver.findElement(By.xpath("//*[@text='5']")).click();
+        driver.findElement(MobileBy.AccessibilityId("plus")).click();
         Thread.sleep(3000);
 
         driver.closeApp();
