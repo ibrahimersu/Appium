874483d639e6139919ff4afcc4b2eda2120d94c7
Vasyl Fomiuk <vasya23fomiuk@gmail.com>
2021-12-21 14:04:06 -0500

    update

--- src/test/java/step_definitions/LoginStepDefinitions.java
+++ src/test/java/step_definitions/LoginStepDefinitions.java
@@ -1,4 +1,12 @@
 package step_definitions;
 
+import io.cucumber.java.en.Given;
+
 public class LoginStepDefinitions {
+
+
+    @Given("print hello world")
+    public void print_hello_world() {
+
+    }
 }
