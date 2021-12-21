f057ec87d79a875f27212f926fd35c41fd629276
Vasyl Fomiuk <vasya23fomiuk@gmail.com>
2021-12-21 15:53:44 -0500

    Update HomePage.java

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -5,8 +5,10 @@ import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 import org.openqa.selenium.support.PageFactory;
 import utils.Driver;
+import utils.MobileUtils;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class HomePage {
     @AndroidFindBy(id = "com.etsy.android:id/search_src_text")
@@ -23,5 +25,23 @@ public class HomePage {
 
     public HomePage(){
         PageFactory.initElements(new AppiumFieldDecorator(Driver.getDriver()), this);
+    }
+
+    public void searchFor(String text) {
+        MobileUtils.waitFor(4000);
+        MobileUtils.clickWithWait(searchIcon);
+
+        MobileUtils.waitFor(2000);
+        searchInput.sendKeys(text);
+
+        MobileUtils.waitFor(2000);
+        prepopulatedSearchResults.get(0).click();
+
+        MobileUtils.waitFor(4000);
+    }
+
+    public List<String> getSearchResultTexts(){
+        MobileUtils.waitFor(4000);
+        return searchResults.stream().map(MobileElement::getText).collect(Collectors.toList());
     }
 }
