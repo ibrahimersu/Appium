bce4d8b647829dbaff09406a446440845ecb798d
Vasyl Fomiuk <vasya23fomiuk@gmail.com>
2021-12-21 10:27:48 -0500

    Create Driver.java

---
+++ src/test/java/utils/Driver.java
@@ -0,0 +1,43 @@
+package utils;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Driver {
+    private static AppiumDriver<MobileElement> driver;
+
+    private Driver() {
+    }
+
+    public static AppiumDriver<MobileElement> getDriver() {
+        if (driver == null) {
+            DesiredCapabilities caps = new DesiredCapabilities();
+            caps.setCapability("deviceName", "Pixel 3");
+            caps.setCapability("platformName", "Android");
+            caps.setCapability("platformVersion", "10");
+            caps.setCapability("automationName", "UiAutomator2");
+            caps.setCapability("app", "https://cybertek-appium.s3.amazonaws.com/etsy.apk");
+            URL url = null;
+            try {
+                url = new URL("http://localhost:4723/wd/hub");
+                driver = new AndroidDriver<>(url, caps);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+                throw new RuntimeException("Appium server URL is not valid!");
+            }
+        }
+        return driver;
+    }
+
+    public static void closeDriver() {
+        if (driver != null) {
+            driver.closeApp();
+            driver = null;
+        }
+    }
+}
