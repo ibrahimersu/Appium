be6701f6fb0b0a26c11016eb00afa39897d208f1
Vasyl Fomiuk <vasya23fomiuk@gmail.com>
2021-12-21 14:48:18 -0500

    Update LoginPage.java

--- src/test/java/pages/LoginPage.java
+++ src/test/java/pages/LoginPage.java
@@ -3,11 +3,12 @@ package pages;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.pagefactory.AndroidFindBy;
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.support.PageFactory;
 import utils.Driver;
+import utils.MobileUtils;
 
 public class LoginPage {
-
     @AndroidFindBy(id = "com.etsy.android:id/edit_username")
     private MobileElement username;
 
@@ -24,5 +25,19 @@ public class LoginPage {
         //PageFactory - we use it to enable @FindBy
         //AppiumFieldDecorator - we use it to enable mobile annotations: @AndroidFindBy, iOSXCUITFindBy  ...
         PageFactory.initElements(new AppiumFieldDecorator(Driver.getDriver()), this);
+    }
+
+    public void clickGetStarted(){
+        MobileUtils.clickWithWait(getStartedBtn);
+    }
+
+    public void login(){
+        String username = "";
+        String password = "";
+        MobileUtils.waitFor(4000);
+        this.username.sendKeys(username);
+        this.password.sendKeys(password);
+        MobileUtils.clickWithWait(signInBtn);
+        MobileUtils.waitFor(4000);
     }
 }
