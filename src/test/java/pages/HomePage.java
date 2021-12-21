b9d5243272169ca0626b6b637098435ab11b7802
Vasyl Fomiuk <vasya23fomiuk@gmail.com>
2021-12-21 15:44:24 -0500

    updates

--- src/test/java/pages/HomePage.java
+++ src/test/java/pages/HomePage.java
@@ -1,4 +1,24 @@
 package pages;
 
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.pagefactory.AndroidFindBy;
+import io.appium.java_client.pagefactory.AppiumFieldDecorator;
+import org.openqa.selenium.support.PageFactory;
+import utils.Driver;
+
+import java.util.List;
+
 public class HomePage {
+    @AndroidFindBy(id = "com.etsy.android:id/search_src_text")
+    private MobileElement searchInput;
+
+    @AndroidFindBy(accessibility = "Show Navigation Drawer")
+    private MobileElement searchIcon;
+
+    @AndroidFindBy(id = "com.etsy.android:id/listing_title")
+    private List<MobileElement> searchResults;
+
+    public HomePage(){
+        PageFactory.initElements(new AppiumFieldDecorator(Driver.getDriver()), this);
+    }
 }
